3750c9c47041619c5e47de7d2b7f18b010df9de6
Muhammad Umar <mu8494759@gmail.com>
2024-05-28 16:08:53 +0500

    darkmode switch added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -68,13 +68,49 @@ export default function Navbar(props) {
             </button>
           </nav>
         </div>
-        <span className="cursor-pointer" onClick={toggleDarkMode}>
+        {/* <span className="cursor-pointer" onClick={toggleDarkMode}>
           {darkMode ? (
             <IoMdMoon className="w-6 h-6" />
           ) : (
             <LuSun className="w-6 h-6" />
           )}
-        </span>
+        </span> */}
+        <div class="flex flex-row justify-between toggle">
+          <label
+            htmlFor="dark-toggle"
+            className="flex items-center cursor-pointer"
+          >
+            <div className="relative">
+              <input
+                type="checkbox"
+                name="dark-mode"
+                id="dark-toggle"
+                className="checkbox hidden"
+                onChange={toggleDarkMode}
+                checked={darkMode}
+              />
+              <div
+                className="block border-[1px] dark:border-white border-gray-900 w-14 h-8 rounded-full"
+                style={{ backgroundColor: darkMode ? "#333" : "transparent" }}
+              ></div>
+              <div
+                className="dot absolute left-1 top-1  w-6 h-6 rounded-full transition"
+                style={{
+                  transform: darkMode ? "translateX(100%)" : "translateX(0%)",
+                }}
+              >
+                {darkMode ? (
+                  <IoMdMoon className="w-6 h-6" />
+                ) : (
+                  <LuSun className="w-6 h-6 " />
+                )}
+              </div>
+            </div>
+            <div className="ml-3" style={{ color: darkMode ? "#fff" : "#333" }}>
+              {darkMode ? `Dark` : "Light"} Mode
+            </div>
+          </label>
+        </div>
         <div className="md:hidden">
           <button
             onClick={() => setIsOpen(!isOpen)}
