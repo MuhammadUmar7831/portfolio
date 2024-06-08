7bbfc84da79e94bc8a0f23aa3f7571710feccc40
Muhammad Umar <mu8494759@gmail.com>
2024-06-08 13:59:19 +0500

    default mode dark

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import DarkMode from "./components/DarkMode";
 import Menu from "./components/Menu";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <>
       <RefProvider>
